ebebe7ca82c625c7ec5b8695b08d9f7671e43c01
Jeremi Plazas <keisuru@gmail.com>
2021-04-26 14:11:37 -0600

    more

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+
+
+
+
+// Custom
+var cssLink = document.createElement("link");
+cssLink.href = "src/css/main.css"; 
+cssLink.rel = "stylesheet"; 
+cssLink.type = "text/css"; 
+frames['rssfeed_frame'].document.head.appendChild(cssLink);
