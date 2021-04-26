9ba13aa9d45c75a8c8524974ceb64c9d25131d65
Jeremi Plazas <keisuru@gmail.com>
2021-04-26 15:18:20 -0600

    asdf

--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ cssLink.href = "src/css/main.css";
 cssLink.rel = "stylesheet"; 
 cssLink.type = "text/css"; 
 frames['rssfeed_frame'].document.head.appendChild(cssLink);
+
+
+// Styling iframe content
+$('a[href^="http://www.democracynow.org/"]').prepend('DN!: ').css('color','#f44336 !important');
+$('a[href^="http://feeds.propublica.org/"]').prepend('PP: ').css('color','#00bcd4 !important');
