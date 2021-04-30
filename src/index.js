909b55fe710b1fbe5b8837697665d261159fbc13
Jeremi Plazas <keisuru@gmail.com>
2021-04-29 21:45:41 -0600

    asdf

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import "./css/main.css";
 console.log("🦊 Hello! Custom JS here...");
 
 
-
-// Styling iframe content
-$('.feed-container a[href^="http://www.democracynow.org/"]').prepend('🗽 DN!: ').css('color','#f44336');
-$('.feed-container a[href^="http://feeds.propublica.org/"]').prepend('🔍 PP: ').css('color','#00bcd4');
-$('.feed-container a[href^="https://theanarchistlibrary.org/"]').prepend('✊🏽 AL: ').css('color','#8bc34a');
+var scriptTag = '<script src="rss.iframe.js"><\/script>';
+$('iframe[name="rssfeed_frame"]').contents().find("body").append(scriptTag);
+  
---
+++ src/rss.iframe.js
@@ -0,0 +1,5 @@
+// Styling iframe content
+
+$('.feed-container a[href^="http://www.democracynow.org/"]').prepend('🗽 DN!: ').css('color','#f44336');
+$('.feed-container a[href^="http://feeds.propublica.org/"]').prepend('🔍 PP: ').css('color','#00bcd4');
+$('.feed-container a[href^="https://theanarchistlibrary.org/"]').prepend('✊🏽 AL: ').css('color','#8bc34a');
